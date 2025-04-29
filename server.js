457844483c9fcc4c7db7a0f33142e10ed7e0543b
Gabriel Feipe <gabrielferreiro963@gmail.com>
2025-04-29 00:33:27 -0300

    db autentication made

--- server.js
+++ server.js
@@ -1,15 +1,40 @@
 const server = require("fastify")()
+const {Pool} = require("pg")
 
 console.log("Servidor no Ar!")
 
-server.get("/get/:user", (request, reply) =>{
-    const {user} = request.params
+    const db = new Pool({
+        user: "postgres",
+        host: "localhost",
+        database: "saloon_dashboard",
+        password: "2006",
+        port: "5432"
+    })
+
+    const data = async () => {
+        const {rows} = await db.query("SELECT CURRENT_DATE")
+        console.log(rows[0])
+    }
+
+    data()
+
+server.get("/get/:user", async (request, reply) =>{
+    let {user} = request.params
+    user = user.toLowerCase()
     const {token} = request.query
     
     console.log(user)
     console.log(token)
 
-    if(user == "Gabriel" && token =="1234"){
+    const query = "SELECT nome, token FROM proprietarios WHERE nome = $1 AND token = $2"
+
+    const {rows} = await db.query(query, [user, token])
+
+    console.log("ROWS:")
+    console.log(rows)
+
+    // como zero e false, entao se a array nao retornar nada ele nao exibe
+    if(rows.length){
         reply.send({acesso: true})
     }else{
         reply.send({acesso: false})
