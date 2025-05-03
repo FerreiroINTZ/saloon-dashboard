5a9c2a0e410f4387a80950d0662ad25e67b37281
Gabriel Feipe <gabrielferreiro963@gmail.com>
2025-05-03 02:14:11 -0300

    histori section and basic featrues made

---
+++ src/app/historico/actionBTNs.js
@@ -0,0 +1,45 @@
+"use client"
+
+import style from "./historico.module.css"
+import { useRouter } from "next/navigation"
+
+function BTNs({id}) {
+  const router = useRouter()
+
+  console.log("id: ", id)
+
+  async function changeState(vall){
+    console.log("Mudancas serao feitas")
+    console.log(vall)
+    console.log(id)
+    const response = await fetch(`http://localhost:3001/setStatus/${id}?estatus=${vall}`)
+    console.log("servicor acessado")
+    
+    const data = await response.json()
+    console.log("data: ")
+    console.log(data)
+    
+    router.refresh()
+  }
+
+  return (
+    <div className={style["actBTNs"]}>
+      <button 
+        className={style["actBTN-imgs"]} 
+        style={{background: "green"}}
+        onClick={(e) => changeState('completo')}><div id={style.complete}></div></button>
+      <button 
+        value="cancelar"
+        className={style["actBTN-imgs"]} 
+        style={{background: "red"}}
+        onClick={(e) => changeState('cancelado')}><div id={style.cancel}></div></button>
+      <button 
+        value="descosiderar"
+        className={style["actBTN-imgs"]} 
+        style={{background: "rgb(50, 60, 231)"}}
+        onClick={(e) => changeState('agendado')}><div id={style.desconsiderar}></div></button>
+    </div>
+  )
+}
+
+export default BTNs
--- src/app/historico/historico.js
+++ src/app/historico/historico.js
@@ -1,6 +1,7 @@
 // import style from "./historico.module.css"
-import style from "../agendamentos/agendamentos.module.css"
+import style from "./historico.module.css"
 import Filtros from "../agend_historico-filtro/filtros"
+import ActionsBTNs from "./actionBTNs"
 
 async function historico({nome, token}) {
 
@@ -12,27 +13,43 @@ async function historico({nome, token}) {
         body: JSON.stringify({nome, token})
     })
 
-    let historico = []
-    // historico = await response.json()
+    const historicoList = await response.json()
 
-    console.log(historico)
+    const agora = new Date();
+
+    // Opções para exibir o nome do mês e outros detalhes
+    const opcoes = {
+      weekday: 'long',  // dia da semana (ex: segunda-feira)
+      month: 'long',    // mês por extenso (ex: maio)
+      day: 'numeric',   // dia do mês (ex: 1)
+      hour: '2-digit',  // hora (ex: 14)
+      minute: '2-digit' // minuto (ex: 05)
+    };
+    
+    // Formata a data com base no idioma (aqui: pt-BR para português do Brasil)
+    const dataFormatada = agora.toLocaleString('pt-BR', opcoes);
+
+    console.log("historicoList")
+    console.log(response.status)
+    console.log(historicoList)
   return (
     <main className="mains">
         <div className="title">
         <h1>Historico de Agendamentos</h1>
-        <p className="today">{"dataFormatada"}</p>
+        <p className="today">{dataFormatada}</p>
       </div>
       <section className="sections">
         <div className="filter-list">
           <Filtros servicos={[]} tempo={""} ordem={""}/>
           <div className="list">
-            <ul>
-              {historico?.map((x, index) =>
-                <li key={index}>
+            <ul id={style.ulHistory}>
+              {historicoList?.map((x, index) =>
+                <li key={index} style={x.estatus == "completo" ? {backgroundColor: "var(--servico-completo)"} : x.estatus == "cancelado" ? {backgroundColor: "var(--servico-incompleto)"} : null}>
                   <p className="lista-nome">{x.nome}</p>
                   <p className="lista-telefone">{x.telefone}</p>
                   <p className="lista-servico">{x.servico}</p>
                   <p className="lista-horario">{x.data_agendamento.slice(5, 10).replace(/-/, "/")}</p>
+                  <ActionsBTNs id={x.id}/>
                 </li>
               )}
             </ul>
