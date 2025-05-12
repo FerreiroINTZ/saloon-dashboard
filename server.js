3cb6c6e0b53dfbb0f72fb778bdbf701aba43ff90
Gabriel Feipe <gabrielferreiro963@gmail.com>
2025-05-11 23:29:55 -0300

    filtro por token do proprietario adcionado

--- server.js
+++ server.js
@@ -1,282 +1,299 @@
-const server = require("fastify")()
-const {Pool} = require("pg")
-const cookies = require("@fastify/cookie")
-const cors = require("@fastify/cors")
+const server = require("fastify")();
+const { Pool, types } = require("pg");
+const cookies = require("@fastify/cookie");
+const cors = require("@fastify/cors");
 
-console.log("Servidor no Ar!")
+console.log("Servidor no Ar!");
 
-    server.register(cookies)
+server.register(cookies);
 
-    server.register(cors, {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"],
-        credentials: true
-    })
+server.register(cors, {
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST"],
+  credentials: true,
+});
 
-    const db = new Pool({
-        user: "postgres",
-        host: "localhost",
-        database: "saloon_dashboard",
-        password: "2006",
-        port: "5432"
-    })
+// types.setTypeParser(114, (val) => val);
 
-    const data = async () => {
-        const {rows} = await db.query("SELECT CURRENT_DATE")
-        console.log(rows[0])
-    }
+// O OID 3802 corresponde ao tipo JSONB
+// types.setTypeParser(3802, (val) => val);
 
-    data()
+const db = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "saloon_dashboard",
+  password: "2006",
+  port: "5432",
+  client_encoding: "UTF8",
+});
 
-server.get("/get/:user", async (request, reply) =>{
+db.connect().then(() => db.query("SET client_encoding  = 'UTF8';"))
 
-    const {cookies} = request
 
-    console.log("cookies: ")
-    console.log(cookies)
+const data = async () => {
+  const { rows } = await db.query("SHOW CLIENT_ENCODING;");
+  console.log(rows[0]);
+};
 
-    let {user} = request.params
-    user = user.toLowerCase()
-    const {token} = request.query
-    
-    console.log(user)
-    console.log(token)
+data();
 
-    const query = "SELECT nome, token FROM proprietarios WHERE nome = $1 AND token = $2"
+// autenticacao do dasboard
+server.get("/get/:user", async (request, reply) => {
+  const { cookies } = request;
 
-    const {rows} = await db.query(query, [user, token])
+  console.log("cookies: ");
+  console.log(cookies);
 
-    console.log("ROWS:")
-    console.log(rows)
+  let { user } = request.params;
+  user = user.toLowerCase();
+  const { token } = request.query;
 
-    // todas as autenticacoes sao feitas aqui
-    if(rows.length){
-        console.log("Acesso:")
-        console.error("Aceito")
-        reply.send({acess: true})
-    }else{
-        console.log("Acesso:")
-        console.error("negado")
-        reply.send({acess: false})
-    }
+  console.log(user);
+  console.log(token);
 
-})
+  const query =
+    "SELECT nome, token FROM proprietarios WHERE nome = $1 AND token = $2";
 
-server.get("/getResumo", async (request, reply) =>{
-    console.log("\nResumo\n")
+  const { rows } = await db.query(query, [user, token]);
 
-    const {nome} = request.query
-    const {token} = request.query
-    console.log(nome)
-    console.log(token)
-    
-    const queryFilters = {
-        columns: "clientes.telefone, clientes.nome, horario, servico_valor.servico, estatus, agendamentos.id",
-        join: "JOIN clientes ON clientes.telefone = client_id JOIN servico_valor ON servico_valor. id = servico_valor",
-        data: "data_agendamento = CURRENT_DATE"
-    }
-    
-    // Fora que ele filtra para os agendamentos de Hoje
-    const query = `SELECT ${queryFilters.columns} FROM agendamentos ${queryFilters.join} WHERE ${queryFilters.data}`
-    // pesquisa os clientes com agendamento para hoje
-    // Dados da pesquisa:
-    // Nome do cliente
-    // Horario
-    // Servico
-    // Telefone
-    
-    console.log(query)
-    const {rows} = await db.query(query)
-    reply.send(rows)
-})
+  console.log("ROWS:");
+  console.log(rows);
 
-server.post("/getAgendamentos", async (request, reply) =>{
-    
-    console.log("\nAgendamentos\n")
-    
-    const {nome} = request.body
-    const {token} = request.body
+  // todas as autenticacoes sao feitas aqui
+  if (rows.length) {
+    console.log("Acesso:");
+    console.error("Aceito");
+    reply.send({ acess: true });
+  } else {
+    console.log("Acesso:");
+    console.error("negado");
+    reply.send({ acess: false });
+  }
+});
 
-    let {servico} = request.body
-    let {tempo} = request.body
-    let {ordem} = request.body
-    
-    // caso nao tenha filtro de tempo, ou seja, valor = HOJE, entao ele nao filtra, praticamnte.
-    if(!tempo){
-        tempo = "0"
-    }else{
-        tempo = Number(tempo) * 7
-    }
+// dados do resumo do dashboard
+server.get("/getResumo", async (request, reply) => {
+  console.log("\nResumo\n");
 
-    if(!servico){
-        servico = "servico_valor.servico"
-    }
+  const { nome } = request.query;
+  const { token } = request.query;
+  console.log(nome);
+  console.log(token);
 
-    if(ordem == "normal" || !ordem){
-        // crescente
-        ordem = "ASC"
-        console.log("FOI!")
-    }else{
-        // descrescente
-        console.log("DESCRESCENTE!")
-        ordem = "DESC"
-    }
+  const queryFilters = {
+    columns:
+      "clientes.telefone, clientes.nome, horario, servico_valor.servico, estatus, agendamentos.id",
+    join: "JOIN clientes ON clientes.telefone = client_id JOIN servico_valor ON servico_valor. id = servico_valor JOIN clientela ON clientela.id = clientela_id",
+    data: "data_agendamento = CURRENT_DATE",
+  };
 
-    const queryFields = {
-        columns: "clientes.telefone, clientes.nome, data_agendamento, servico_valor.servico, servico_valor.valor",
-        join: "JOIN clientes ON clientes.telefone = client_id JOIN servico_valor ON servico_valor. id = servico_valor",
+  // Fora que ele filtra para os agendamentos de Hoje
+  const query = `SELECT ${queryFilters.columns} FROM agendamentos ${queryFilters.join} WHERE ${queryFilters.data} AND clientela.poriorietario_id = ${token}`;
+  // pesquisa os clientes com agendamento para hoje
+  // Dados da pesquisa:
+  // Nome do cliente
+  // Horario
+  // Servico
+  // Telefone
 
-        // CURRENT_DATE + INTERVAL: '7 days' => faz um calculo. O INTERVAL serve para adicionar masi dias.
-        date: `data_agendamento <= CURRENT_DATE + INTERVAL '${tempo} days' AND data_agendamento >= CURRENT_DATE`,
-        
-        // ORDER BY <coluna> ASC -> ordena em ordem crescente ou alfabetica.
-        ordem: `ORDER BY data_agendamento ${ordem}`,
-    }
+  console.log(query);
+  const { rows } = await db.query(query);
+  reply.send(rows);
+});
 
-    const query = `SELECT ${queryFields.columns} FROM agendamentos ${queryFields.join} WHERE ${queryFields.date} ${queryFields.ordem}`
-    
-    console.log("pesquisando!")
+// dados de agendamentos do dashboard
+server.post("/getAgendamentos", async (request, reply) => {
+  console.log("\nAgendamentos\n");
 
-        const {rows} = await db.query(query)
-        
-        console.log("rows:")
-        // console.log(rows)
-        console.log(ordem)
+  const { nome } = request.body;
+  const { token } = request.body;
 
-    reply.send(rows)
-})
+  let { servico } = request.body;
+  let { tempo } = request.body;
+  let { ordem } = request.body;
 
-server.post("/getHistory", async (request,reply) =>{
-    console.log("Get History ")
-    
-    const {nome} = request.body
-    const {token} = request.body
+  // caso nao tenha filtro de tempo, ou seja, valor = HOJE, entao ele nao filtra, praticamnte.
+  if (!tempo) {
+    tempo = "0";
+  } else {
+    tempo = Number(tempo) * 7;
+  }
 
-    let {tempo} = request.body
-    let {ordem} = request.body
-    let {servico} = request.body
+  if (!servico) {
+    servico = "servico_valor.servico";
+  }
 
-    if(ordem == "normal" || !ordem){
-        ordem = "DESC"
-    }else{
-        ordem = "ASC"
-    }
-    
-    console.log(tempo)
-    if(!tempo){
-        tempo = "1"
-    }else{
-        tempo = Number(tempo) * 7
-    }
+  if (ordem == "normal" || !ordem) {
+    // crescente
+    ordem = "ASC";
+    console.log("FOI!");
+  } else {
+    // descrescente
+    console.log("DESCRESCENTE!");
+    ordem = "DESC";
+  }
 
-    const queryFields = {
-        columns: "agendamentos.id, clientes.nome, clientes.telefone, servico_valor.servico, data_agendamento, estatus",
-        join: "JOIN clientes ON clientes.telefone = client_id JOIN servico_valor ON servico_valor.id = servico_valor",
+  const queryFields = {
+    columns:
+      "clientes.telefone, clientes.nome, data_agendamento, servico_valor.servico, servico_valor.valor",
+    join: "JOIN clientes ON clientes.telefone = client_id JOIN servico_valor ON servico_valor. id = servico_valor JOIN clientela ON clientela.id = clientela_id",
 
-        // CURRENT_DATE + INTERVAL: '7 days' => faz um calculo. O INTERVAL serve para adicionar masi dias.
-        date: `data_agendamento >= CURRENT_DATE - INTERVAL '${tempo} days' AND data_agendamento < CURRENT_DATE`,
-        
-        // ORDER BY <coluna> ASC -> ordena em ordem crescente ou alfabetica.
-        ordem: `ORDER BY data_agendamento ${ordem}`,
-    }
+    // CURRENT_DATE + INTERVAL: '7 days' => faz um calculo. O INTERVAL serve para adicionar masi dias.
+    date: `data_agendamento <= CURRENT_DATE + INTERVAL '${tempo} days' AND data_agendamento >= CURRENT_DATE AND clientela.proprietario_id = ${token}`,
 
-    const query = `SELECT ${queryFields.columns} FROM agendamentos ${queryFields.join} WHERE ${queryFields.date} ${queryFields.ordem}`
+    // ORDER BY <coluna> ASC -> ordena em ordem crescente ou alfabetica.
+    ordem: `ORDER BY data_agendamento ${ordem}`,
+  };
 
-    const {rows} = await db.query(query)
-    // console.log(rows)
-    console.log(ordem)
-    console.log(tempo)
+  const query = `SELECT ${queryFields.columns} FROM agendamentos ${queryFields.join} WHERE ${queryFields.date} ${queryFields.ordem}`;
 
-    reply.send(rows)
-})
+  console.log("pesquisando!");
 
-server.get("/setStatus/:id", async (request, reply) =>{
-    console.log("Set estatus")
-    
-    const {id} = request.params
-    const {estatus} = request.query
-    
-    console.log("dados: ")
-    console.log(id)
-    console.log(estatus)
-    
-    const query = `UPDATE agendamentos SET estatus = '${estatus}' WHERE id = ${id}`
-    
-    const {rows} = await db.query(query)
-    
-    console.log("rows: ")
-    console.log(rows)
+  const { rows } = await db.query(query);
 
-    reply.send(rows)
-})
+  console.log("rows:");
+  // console.log(rows)
+  console.log(ordem);
 
-    server.post("/lucros", async (request, reply) =>{
-        console.log("\nLucros\n")
+  reply.send(rows);
+});
 
-        // const {nome, token} = request.body
+// dados do jistorico do dashbaord
+server.post("/getHistory", async (request, reply) => {
+  console.log("Get History ");
 
-        let {tempo} = request.body
-        let {estado} = request.body
+  const { nome } = request.body;
+  const { token } = request.body;
 
-        console.log("Vars: ")
-        console.log(tempo)
-        console.log(estado)
-        
-        if(!tempo || typeof(tempo) === "undefined" || tempo == "ontem"){
-            console.log("Nada!")
-            time = 1
-        }else{
-            console.log("Tudo!")
-            time = Number(tempo) * 7
-        }
+  let { tempo } = request.body;
+  let { ordem } = request.body;
+  let { servico } = request.body;
 
-        if(!estado || estado == "todos"){
-            estado = "estatus != 'agendado'"
-        }else{
-            estado = `estatus = '${estado}'`
-        }
-        console.log("Apos a mudanca: ")
-        console.log(tempo)
-        console.log(estado)
+  if (ordem == "normal" || !ordem) {
+    ordem = "DESC";
+  } else {
+    ordem = "ASC";
+  }
 
-        const queryFilters = {
-            columns: "c.nome, c.telefone, sv.servico, sv.valor, estatus",
-            join: "JOIN clientes c ON c.telefone = client_id JOIN servico_valor sv ON sv.id = servico_valor",
-            data: `data_agendamento <= CURRENT_DATE AND data_agendamento >= CURRENT_DATE - INTERVAL '${time} days'`
-        }
+  console.log(tempo);
+  if (!tempo) {
+    tempo = "1";
+  } else {
+    tempo = Number(tempo) * 7;
+  }
 
-        const query = `SELECT ${queryFilters.columns} FROM agendamentos ${queryFilters.join} WHERE ${estado} AND ${queryFilters.data}`
+  const queryFields = {
+    columns:
+      "agendamentos.id, clientes.nome, clientes.telefone, servico_valor.servico, data_agendamento, estatus",
+    join: "JOIN clientes ON clientes.telefone = client_id JOIN servico_valor ON servico_valor.id = servico_valor JOIN clientela ON clientela.id = clientela_id",
 
-        console.log(query)
-        
-        const {rows} = await db.query(query)
-        
-        console.log("rows: ")
-        console.log(rows[0])
-        
-        reply.send(rows)
-    })
-    
-    server.get("/getFreeDays", async (request, reply) => {
-        console.log("\nGet Free Days\n");
-    
-        let tempoBruto = request.query?.tempo;
-        let tempo = Number(tempoBruto);
-    
-        console.log("tempo bruto:", tempoBruto);
-        console.log("convertido:", tempo);
-    
-        if (isNaN(tempo) || tempo <= 0) {
-            tempo = 1;
-        }
-    
-        if (tempo > 1) {
-            tempo *= 7;
-        }
-    
-        console.log("tempo final (em dias):", tempo);
-    
-        const query = `
+    // CURRENT_DATE + INTERVAL: '7 days' => faz um calculo. O INTERVAL serve para adicionar masi dias.
+    date: `data_agendamento >= CURRENT_DATE - INTERVAL '${tempo} days' AND data_agendamento < CURRENT_DATE AND clientela.proprietario_id = ${token}`,
+
+    // ORDER BY <coluna> ASC -> ordena em ordem crescente ou alfabetica.
+    ordem: `ORDER BY data_agendamento ${ordem}`,
+  };
+
+  const query = `SELECT ${queryFields.columns} FROM agendamentos ${queryFields.join} WHERE ${queryFields.date} ${queryFields.ordem}`;
+
+  const { rows } = await db.query(query);
+  // console.log(rows)
+  console.log(ordem);
+  console.log(tempo);
+
+  reply.send(rows);
+});
+
+// muda o esatus de um agendametno, no dashboard (agendado, completo, cancelado)
+server.get("/setStatus/:id", async (request, reply) => {
+  console.log("Set estatus");
+
+  const { id } = request.params;
+  const { estatus } = request.query;
+
+  console.log("dados: ");
+  console.log(id);
+  console.log(estatus);
+
+  const query = `UPDATE agendamentos SET estatus = '${estatus}' WHERE id = ${id}`;
+
+  const { rows } = await db.query(query);
+
+  console.log("rows: ");
+  console.log(rows);
+
+  reply.send(rows);
+});
+
+// dados dos lucrso, do dashboard
+server.post("/lucros", async (request, reply) => {
+  console.log("\nLucros\n");
+
+  // const {nome, token} = request.body
+
+  let { tempo } = request.body;
+  let { estado } = request.body;
+
+  console.log("Vars: ");
+  console.log(tempo);
+  console.log(estado);
+
+  if (!tempo || typeof tempo === "undefined" || tempo == "ontem") {
+    console.log("Nada!");
+    time = 1;
+  } else {
+    console.log("Tudo!");
+    time = Number(tempo) * 7;
+  }
+
+  if (!estado || estado == "todos") {
+    estado = "estatus != 'agendado'";
+  } else {
+    estado = `estatus = '${estado}'`;
+  }
+  console.log("Apos a mudanca: ");
+  console.log(tempo);
+  console.log(estado);
+
+  const queryFilters = {
+    columns: "c.nome, c.telefone, sv.servico, sv.valor, estatus",
+    join: "JOIN clientes c ON c.telefone = client_id JOIN servico_valor sv ON sv.id = servico_valor JOIN clientela ON clientela.id = clientela_id",
+    data: `data_agendamento <= CURRENT_DATE AND data_agendamento >= CURRENT_DATE - INTERVAL '${time} days'`,
+  };
+
+  const query = `SELECT ${queryFilters.columns} FROM agendamentos ${queryFilters.join} WHERE ${estado} AND ${queryFilters.data} AND clientela.proprietario_id = ${token}`;
+
+  console.log(query);
+
+  const { rows } = await db.query(query);
+
+  console.log("rows: ");
+  console.log(rows[0]);
+
+  reply.send(rows);
+});
+
+// dados dos dias livres, dos agendamentos do dashboard
+server.get("/getFreeDays", async (request, reply) => {
+  console.log("\nGet Free Days\n");
+
+  let tempoBruto = request.query?.tempo;
+  let tempo = Number(tempoBruto);
+
+  console.log("tempo bruto:", tempoBruto);
+  console.log("convertido:", tempo);
+
+  if (isNaN(tempo) || tempo <= 0) {
+    tempo = 1;
+  }
+
+  if (tempo > 1) {
+    tempo *= 7;
+  }
+
+  console.log("tempo final (em dias):", tempo);
+
+  const query = `
             WITH dias AS (
                 SELECT generate_series(
                     CURRENT_DATE, 
@@ -289,14 +306,35 @@ server.get("/setStatus/:id", async (request, reply) =>{
             LEFT JOIN agendamentos a ON d.dia = a.data_agendamento
             WHERE a.data_agendamento IS NULL
         `;
-    
-        const { rows } = await db.query(query);
-    
-        console.log("rows: ");
-        console.log(rows);
-    
-        reply.send(rows);
-    });
-    
 
-server.listen({port: 3001})
+  const { rows } = await db.query(query);
+
+  console.log("rows: ");
+  console.log(rows);
+
+  reply.send(rows);
+});
+
+// recupera os dados da landing page do salao
+server.get("/getClientToken/:token", async (request, reply) => {
+  const { token } = request.params;
+  console.log("token: ");
+  console.log(token);
+
+  const query ="SELECT content, p.modelo FROM site_content JOIN proprietarios p ON p.client_token = proprietario_client_token WHERE proprietario_client_token = $1";
+  // const query ="SELECT texto FROM site_content WHERE id = 10";
+
+  const { rows } = await db.query(query, [token]);
+  // const { rows } = await db.query(query);
+
+  console.log(rows[0]);
+  if(rows[0] === undefined){
+    console.log("dsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsdsd");
+    reply.header("Content-Type", "application/json; charset=utf-8").code(300).send(rows[0]);
+  }
+  console.log("=====================================");
+
+  reply.header("Content-Type", "application/json; charset=utf-8").send(rows[0]);
+});
+
+server.listen({ port: 3001 });
